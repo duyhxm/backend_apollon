664b968876e4993a20c795f4563b814eafe2ec61
duyhxm <2251012049duy@ou.edu.vn>
2024-01-09 13:21:04 +0700

    fix cors 9

--- src/server.js
+++ src/server.js
@@ -13,7 +13,6 @@ app.use(express.json());
 app.use(express.urlencoded());
 configViewEngine(app);
 app.use('/', webRoutes);
-app.use(cors());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
