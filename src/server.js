f5b7cf1f0e5cf751f3e0da8a0602a509902f7766
duyhxm <2251012049duy@ou.edu.vn>
2024-01-09 10:55:49 +0700

    add cors

--- src/server.js
+++ src/server.js
@@ -13,6 +13,12 @@ app.use(express.json());
 app.use(express.urlencoded());
 configViewEngine(app);
 app.use('/', webRoutes);
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://www.duyhxm.me');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 const { OAuth2Client } = require('google-auth-library');
 const nodemailer = require('nodemailer');
